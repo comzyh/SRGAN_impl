1eda7d48564644d1c90d6785c00296be787446cb
Yonghui Zhang <comzyh@gmail.com>
2018-05-30 19:07:14 +0800

    remove estiminator

--- model.py
+++ model.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 
 
+def srresnet_preprocess(images):
+    return (tf.to_float(images) / 127.5) - 1
+
+
+def srresnet_postprocess(images):
+    return (images + 1) * 127.5
+
+
 def SRResNet(images, training, reuse=False, residual_blocks_num=16):
     """
     Ledig, C., Theis, L., Huszár, F., Caballero, J., Cunningham, A., Acosta, A., … Shi Twitter, W. (n.d.).
@@ -73,3 +81,36 @@ def SRResNet(images, training, reuse=False, residual_blocks_num=16):
             name='k9n3s1'
         )
     return x
+
+
+def SRGAN_discriminator(images, training, reuse=False):
+
+    images = tf.image.resize_bicubic(images, (96, 96))
+    with tf.variable_scope("SRGAN_D"):
+        x = tf.layers.conv2d(
+            inputs=images, filters=64, kernel_size=(3, 3), strides=(1, 1),
+            padding='same', use_bias=False, reuse=reuse, name='conv1',
+        )
+        x = tf.nn.leaky_relu(x)
+
+        # blocks
+        filter_list = [64, 128, 128, 256, 256, 512, 512]
+        stride_list = [2, 1, 2, 1, 2, 1, 2]
+        for f, s in zip(filter_list, stride_list):
+            x = tf.layers.conv2d(
+                inputs=x, filters=f, kernel_size=(3, 3), strides=(2, 2),
+                padding='same', use_bias=False, reuse=reuse,
+                name='k3n{}s{}/conv'.format(f, s)
+            )
+            x = tf.layers.batch_normalization(
+                inputs=x, axis=3, fused=True, scale=True,
+                training=training, reuse=reuse,
+                name='k3n{}s{}/bn'.format(f, s)
+            )
+            x = tf.nn.leaky_relu(x)
+        x = tf.layers.flatten(x, name='flatten')
+        x = tf.layers.dense(x, units=1000, reuse=reuse, name='dense1')
+        x = tf.nn.leaky_relu(x)
+        logits = tf.layers.dense(x, units=1, reuse=reuse, name='dense2')
+        confidence = tf.sigmoid(logits)
+        return logits, confidence
--- train.py
+++ train.py
@@ -4,9 +4,13 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.python.ops import math_ops
-
+from tensorflow.python.framework.errors_impl import OutOfRangeError
 
 from model import SRResNet
+from model import SRGAN_discriminator
+from model import srresnet_preprocess
+from model import srresnet_postprocess
+
 
 
 def parse_record(record):
@@ -36,42 +40,30 @@ def get_dataset(tfrecord_dir, setname):
     return dataset
 
 
-def model_fn(features, labels, mode):
-    trainig = (mode == tf.estimator.ModeKeys.TRAIN)
-    lr_images = features['lr_images']
-    lr_images = tf.to_float(lr_images) / 127.5 - 1
+def evalue(datapath):
+    dataset = get_dataset(datapath, 'validation')
+    iterator = dataset.make_one_shot_iterator()
+    next_element = iterator.get_next()
+    with tf.Session() as sess:
+        # def get_next_figure():
+        #     hr_image, lr_image = sess.run(next_element)
 
-    x = SRResNet(lr_images, training=trainig)
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = {"sr_images": tf.saturate_cast((x + 1) * 127.5, dtype=tf.uint8)}
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-    hr_images = features['hr_images']
-    hr_images = tf.to_float(hr_images) / 127.5 - 1
-    mse = tf.losses.mean_squared_error(labels=hr_images, predictions=x)
-    loss = mse
-    # PSNR
-    max_value = 2.0
-    psnr = math_ops.subtract(
-        20 * math_ops.log(max_value) / math_ops.log(10.0),
-        np.float32(10 / np.log(10)) * math_ops.log(mse),
-        name='psnr')
-    psnr = tf.metrics.mean(psnr)
-
-    tf.summary.scalar('psnr', psnr[1])
-
-    if trainig:
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.00001, name='Adam')
-        train_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS)  # for batch_normalization
-        train_op.append(optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step()))
-        train_op = tf.group(*train_op)
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-    eval_metric_ops = {
-        "psnr": psnr,
-    }
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+        #     def get_input_fn(lr_image):
+        #         def input_fn():
+        #             return {'lr_images': tf.constant(np.expand_dims(lr_image, 0), dtype=tf.float32)}
+        #         return input_fn
+        #     result = next(srresnet.predict(input_fn=get_input_fn(lr_image)))
+        #     # result_image = np.clip(result['sr_images'], 0, 255).astype(np.uint8)
+        #     result_image = result['sr_images']
+        #     plt.subplot(1, 3, 1)
+        #     plt.imshow(hr_image)
+        #     plt.subplot(1, 3, 2)
+        #     plt.imshow(result_image)
+        #     plt.subplot(1, 3, 3)
+        #     plt.imshow(np.clip(lr_image, 0, 255).astype(np.uint8))
+        #     return hr_image, lr_image, result['sr_images']
+        import IPython
+        IPython.embed()
 
 
 def main():
@@ -79,65 +71,104 @@ def main():
     parser.add_argument('--datapath', type=str, required=True, help='location of SR dataset tfrecords')
     parser.add_argument('--model_dir', type=str, default='/tmp/SRResNet', help='directory to save model')
     parser.add_argument('--evalue', action='store_true', help='evalue model')
+    parser.add_argument('--bs', type=int, default=64, help='batch size')
 
     args = parser.parse_args()
 
-    config = tf.estimator.RunConfig(model_dir=args.model_dir,
-                                    save_summary_steps=1,
-                                    save_checkpoints_steps=100,
-                                    keep_checkpoint_max=10,
-                                    log_step_count_steps=10)
-
-    srresnet = tf.estimator.Estimator(model_fn=model_fn, config=config)
-    print(srresnet.latest_checkpoint())
-    batch_size = 64
-
-    def input_fn_factory(setname):
-        def input_fn():
-            dataset = get_dataset(args.datapath, setname)
-            dataset = dataset.repeat(count=10)
-            if setname == 'train':
-                dataset = dataset.shuffle(1000)
-            if setname != 'evalue':
-                dataset = dataset.batch(batch_size)
-            dataset = dataset.prefetch(2)
-            iterator = dataset.make_one_shot_iterator()
-            hr_images, lr_images = iterator.get_next()
-            return {'hr_images': hr_images, 'lr_images': lr_images}
-
-        return input_fn
+    batch_size = args.bs
 
     if args.evalue:
-        dataset = get_dataset(args.datapath, 'validation')
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
-        with tf.Session() as sess:
-            def get_next_figure():
-                hr_image, lr_image = sess.run(next_element)
+        return evalue()
 
-                def get_input_fn(lr_image):
-                    def input_fn():
-                        return {'lr_images': tf.constant(np.expand_dims(lr_image, 0), dtype=tf.float32)}
-                    return input_fn
-                result = next(srresnet.predict(input_fn=get_input_fn(lr_image)))
-                # result_image = np.clip(result['sr_images'], 0, 255).astype(np.uint8)
-                result_image = result['sr_images']
-                plt.subplot(1, 3, 1)
-                plt.imshow(hr_image)
-                plt.subplot(1, 3, 2)
-                plt.imshow(result_image)
-                plt.subplot(1, 3, 3)
-                plt.imshow(np.clip(lr_image, 0, 255).astype(np.uint8))
-                return hr_image, lr_image, result['sr_images']
-            import IPython
-            IPython.embed()
-        return
+    train_dataset = get_dataset(args.datapath, 'train')  # .shuffle(1000)
+    train_dataset = train_dataset.batch(batch_size).prefetch(2)
+    train_iterator = train_dataset.make_initializable_iterator()
 
-    for epoch in range(40):
-        print('Epoch {}'.format(epoch))
-        srresnet.train(input_fn=input_fn_factory('train'))
-        eval_results = srresnet.evaluate(input_fn=input_fn_factory('validation'))
-        print(eval_results)
+    hr_images, lr_images = train_iterator.get_next()
+
+    # build graph
+
+    # hr_images_ph = tf.placeholder(dtype=tf.uint8, shape=(None, 96, 96, 3))
+    # lr_images_ph = tf.placeholder(dtype=tf.float32, shape=(None, 48, 48, 3))
+
+    hr_images = srresnet_preprocess(hr_images)
+    lr_images = srresnet_preprocess(lr_images)
+
+    # SRResNet
+    sr_images = SRResNet(lr_images, training=True, reuse=True)
+
+    # Discriminator
+    d_logits_fake, _ = SRGAN_discriminator(sr_images, training=True, reuse=False)
+    d_logits_real, _ = SRGAN_discriminator(hr_images, training=True, reuse=True)
+
+    # loss to train discriminator
+    d_loss_fake = tf.nn.sigmoid_cross_entropy_with_logits(
+        labels=tf.zeros_like(d_logits_fake), logits=d_logits_fake,
+        name='d_loss_fake'
+    )
+    d_loss_real = tf.nn.sigmoid_cross_entropy_with_logits(
+        labels=tf.ones_like(d_logits_real), logits=d_logits_real,
+        name='d_loss_real'
+    )
+
+    d_loss = d_loss_fake + d_loss_real
+
+    # loss to train SRResNet
+    g_mse_loss = tf.losses.mean_squared_error(labels=hr_images, predictions=sr_images)
+
+    g_gan_loss = tf.nn.sigmoid_cross_entropy_with_logits(
+        # we want gentrator generate images that looks realistic
+        # so labels = 1
+        labels=tf.ones_like(d_logits_fake), logits=d_logits_fake,
+        name='g_gan_loss'
+    )
+    g_loss = g_mse_loss + 0.001 * g_gan_loss
+
+    # varlist
+    g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='SRResNet')
+    d_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='SRGAN_D')
+    p_train_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='SRResNet')
+    g_train_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='SRResNet')
+    d_train_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='SRGAN_D')
+
+    # optimize
+    lr_all = tf.constant(0.00001, dtype=tf.float32)
+    p_optim = tf.train.AdamOptimizer(lr_all, beta1=0.9).minimize(g_mse_loss, var_list=g_vars)
+    g_optim = tf.train.AdamOptimizer(lr_all, beta1=0.9).minimize(g_loss, var_list=g_vars)
+    d_optim = tf.train.AdamOptimizer(lr_all, beta1=0.9).minimize(d_loss, var_list=d_vars)
+
+    saver = tf.train.Saver()
+    model_path = os.path.join(args.model_dir, 'model')
+
+    # start session
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.per_process_gpu_memory_fraction = 0.1
+    with tf.Session(config=config) as sess:
+        # sess.run()
+
+        try:
+            saver.restore(sess, model_path)
+        except Exception:
+            print('initialize parameters')
+            init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+            sess.run(init_op)
+
+        # import IPython
+        # IPython.embed()
+
+        # sess.run(sr_images)
+
+        for epoch in range(40):
+            print("epoch {}".format(epoch))
+            sess.run(train_iterator.initializer)
+            while True:
+                try:
+                    a = sess.run([p_optim, g_mse_loss, tf.shape(lr_images)])
+                    print(a)
+                except OutOfRangeError:
+                    break
+            saver.save(sess, model_path, global_step=tf.train.get_global_step())
 
 
 if __name__ == '__main__':
